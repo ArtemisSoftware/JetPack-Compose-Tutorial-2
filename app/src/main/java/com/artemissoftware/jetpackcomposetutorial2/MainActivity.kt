feb981195fab2a5e3408ba0ecf9f61efd2845db8
gustavo <gustavo.isaac@gmail.com>
2021-08-27 15:50:20 +0100

    Creating an Image Card Composable

--- app/src/main/java/com/artemissoftware/jetpackcomposetutorial2/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/jetpackcomposetutorial2/MainActivity.kt
@@ -3,28 +3,60 @@ package com.artemissoftware.jetpackcomposetutorial2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+
+            val painter = painterResource(id = R.drawable.artemis_1)
+            val description = "Artemis hunts at night"
+            val title = "The hunter goddess"
+
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)) {
+                
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
+
+        }
+
+    }
+
+
+    fun modifiers(){
+        setContent {
             Column(
                 modifier = Modifier
                     .background(Color.Green)
                     .fillMaxHeight(0.5f)
                     .fillMaxWidth()
-                    .border(5.dp, Color.Magenta).padding(top = 50.dp)
-                    .border(15.dp, Color.Cyan).padding(top = 50.dp)
-                    .border(25.dp, Color.White).padding(top = 50.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(top = 50.dp)
+                    .border(15.dp, Color.Cyan)
+                    .padding(top = 50.dp)
+                    .border(25.dp, Color.White)
+                    .padding(top = 50.dp)
 
             ) {
                 Text(text = "Artemis")
@@ -32,7 +64,6 @@ class MainActivity : ComponentActivity() {
                 Text(text = "Software")
             }
         }
-
     }
 
 
@@ -72,3 +103,45 @@ class MainActivity : ComponentActivity() {
 
 }
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+){
+
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ){
+
+
+        
+        Box(modifier = Modifier.height(200.dp)){
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent, Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+
+    }
+}
